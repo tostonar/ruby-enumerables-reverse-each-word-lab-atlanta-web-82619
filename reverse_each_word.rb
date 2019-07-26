7675c8afce0c01cb6b6616199f10959cca6d18dc
Andrea Toston <tostonar@gmail.com>
2019-07-26 13:46:29 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,9 @@
 def reverse_each_word(string)
-   p string.split(" ")
+   string_array = string.split(" ")
+   reversed = string_array.collect do |word|
+     word.reverse
+   end
+   p reversed
 end
 
 reverse_each_word("Hello there, and how are you?")
