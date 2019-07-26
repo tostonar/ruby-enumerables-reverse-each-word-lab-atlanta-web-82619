ab138ba7a1660be08ad6e44c91413ef7135c2fab
Andrea Toston <tostonar@gmail.com>
2019-07-26 13:49:17 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,12 @@
 def reverse_each_word(string)
+  # separate string into array
    string_array = string.split(" ")
+   # reverse each word in place
    reversed = string_array.collect do |word|
      word.reverse
    end
-   p reversed.join(" ")
+   # put array back into a string
+   reversed.join(" ")
 end
 
-reverse_each_word("Hello there, and how are you?")
+
